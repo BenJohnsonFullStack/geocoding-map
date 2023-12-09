942e4800dbd703502d8112042e264dbb8862d0a1
Ben Johnson <monk.sound.design@gmail.com>
2023-12-09 11:39:24 -0500

    full query params wired

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const url = require("url");
 
 const PORT = process.env.PORT || 9000;
 
@@ -14,9 +15,15 @@ app.use(cors());
 // routes
 app.get("/api/search/:query", async (req, res) => {
   try {
+    // add API key and query strings
+    const params = new URLSearchParams({
+      access_token: process.env.API_KEY,
+      ...url.parse(req.url, true).query,
+    });
+    console.log(params);
     const query = req.params.query;
     const results = await axios(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
     );
     const data = results.data;
     res.json(data);
