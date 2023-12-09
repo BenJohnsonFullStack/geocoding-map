3290092c16998eff06f8561e4d6860ca683fac84
Ben Johnson <monk.sound.design@gmail.com>
2023-12-09 11:24:36 -0500

    GET search API by query wired

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 9000;
 
@@ -9,6 +10,19 @@ const app = express();
 
 // enable cors
 app.use(cors());
+
+// routes
+app.get("/api/search/:query", async (req, res) => {
+  try {
+    const query = req.params.query;
+    const results = await axios(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`
+    );
+    console.log(results.data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
