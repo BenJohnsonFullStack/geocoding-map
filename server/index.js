e72947247697ef6f7c04d431f0e03c99518010c6
Ben Johnson <monk.sound.design@gmail.com>
2023-12-09 11:44:42 -0500

    split out api GET searchResults

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-const url = require("url");
+
+const searchResults = require("./routes/searchResults");
 
 const PORT = process.env.PORT || 9000;
 
@@ -13,24 +13,7 @@ const app = express();
 app.use(cors());
 
 // routes
-app.get("/api/search/:query", async (req, res) => {
-  try {
-    // add API key and query strings
-    const params = new URLSearchParams({
-      access_token: process.env.API_KEY,
-      ...url.parse(req.url, true).query,
-    });
-    console.log(params);
-    const query = req.params.query;
-    const results = await axios(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
-    );
-    const data = results.data;
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+app.use("/api/search", searchResults);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
