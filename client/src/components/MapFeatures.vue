2a3450e81dc5fc60f8e044d5c8440985d8bcda25
Ben Johnson <monk.sound.design@gmail.com>
2023-12-10 11:19:08 -0500

    search bar

--- client/src/components/MapFeatures.vue
+++ client/src/components/MapFeatures.vue
@@ -2,6 +2,18 @@
   <div
     class="w-full md:w-auto absolute md:top-[40px] md:left-[60px] z-[2] flex gap-4 px-6 py-8 md:px-0 md:py-0 bg-transparent"
   >
+    <!-- Search -->
+    <div class="relative flex-1 md:min-w-[350px]">
+      <input
+        type="text"
+        class="pl-9 pr-4 py-3 text-sm focus:outline-slate-600 w-full shadow-md rounded-md"
+        placeholder="Search your favorite spot..."
+      />
+
+      <div class="absolute top-0 left-[8px] h-full flex items-center">
+        <i class="fas fa-search"></i>
+      </div>
+    </div>
     <!-- Geolocation -->
     <div
       class="px-4 bg-white flex items-center shadow-md rounded-md min-h-[45px]"
